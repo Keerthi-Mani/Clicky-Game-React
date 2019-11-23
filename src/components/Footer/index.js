485c72b387fbd955e204548826089eaefb325100
Keerthi Mani <carkeane.getz@gmail.com>
2019-11-22 21:03:08 -0500

    Don't click image more than once

---
+++ src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="bottom">Clicky Game! 2019</div>
+    </footer>
+  );
+}
+
+export default Footer;
